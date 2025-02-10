1b1db0d6d395c65a22442b76514fae2f9036afa1
Antonio Navarro <anavarro67@hotmail.com>
2025-02-10 15:35:56 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use('/', (req: Request, res: Response) => {
+    res.send('Express is working!');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`[Server]: Server is running on port ${process.env.PORT}`);
+})
