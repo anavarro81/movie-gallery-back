12f13daf70ab128247aa0aa5fa9075068ca19b9e
Antonio Navarro <anavarro67@hotmail.com>
2025-02-10 15:51:10 +0100

    subir los cambios

--- dist/app.js
+++ dist/app.js
@@ -7,9 +7,10 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    res.send('Express is working!');
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("Express + TypeScript Server");
 });
-app.listen(process.env.PORT, () => {
-    console.log(`[Server]: Server is running on port ${process.env.PORT}`);
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
